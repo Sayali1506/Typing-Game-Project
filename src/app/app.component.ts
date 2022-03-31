4e588551a48e245d61ff3d780114bb6b208188ed
Sayali Chitale <schitale710@gmail.com>
2022-03-31 17:21:08 +0530

    simple game using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+// lorem.sentence();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Typing-Game';
+  randomText = lorem.sentences();
+  enteredText: string = '';
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
